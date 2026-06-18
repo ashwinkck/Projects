23f25f36325c107fc9079fb87c625045840a8e09
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-18 09:19:21 +0530

    uodassfcf

--- pig-game/project1.py
+++ pig-game/project1.py
@@ -1,4 +1,6 @@
 import random
+print("Welcome to the pig game!")
+print("The max score is 50. Whoever reaches 50 first wins")
 
 def roll():
     min_value = 1
@@ -10,6 +12,8 @@ def roll():
 
 while True:
     players = input("Enter the no of players (2-4): ")
+    if players == "q":
+        exit()
     if players.isdigit():
         players = int(players)
         if 2<=players<=4:
@@ -18,6 +22,28 @@ while True:
             print("Must be between 2-4 players")
     else:
         print("Invalid, try again")
-print(players)        
 
 
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+
+    current_score = 0
+    should_roll = input("Would you like to roll (y)?")
+    if should_roll == "q":
+        break
+    if should_roll.lower() != "y":
+        break
+    
+    value = roll()
+
+    if value == 1:
+        print("You rolled a 1! Turn done!")
+        break
+    else:
+        current_score += value
+        print(f"You rolled a:{value} value ")
+
+
+    print(f"Your score is {current_score}")
