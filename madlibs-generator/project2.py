ea3ee6728410417166494b64aa28c046f03c256b
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-19 23:17:08 +0530

    came_back_to_fix_the_error

--- madlibs-generator/project2.py
+++ madlibs-generator/project2.py
@@ -17,13 +17,3 @@ for i, char in enumerate(story):
         words.append(word)
         start_of_word = -1
 
-answers = {}
-
-for word in words:
-    answer = input("Enter a word for " + word + ": ")
-    answers[word] = answer
-
-for word in words:
-    story = story.replace(word, answers[word])
-
-print(story)
