dcf20a9e8ed6f63555fc7ca6626bcbc4d13c249f
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-24 17:48:51 +0530

    can_get_the_answer_with_if_operator

--- Timed-math-challenge/project3.py
+++ Timed-math-challenge/project3.py
@@ -9,9 +9,16 @@ def generate_problem():
     right = random.randint(MAX_OPERAND,MAX_OPERAND)
     oprerator = random.choice(OPERATORS)
 
+    
+
 
     expr = str(left) + " " + oprerator + " " + str(right)
     print(expr)
+    if oprerator == OPERATORS[0]:
+        answer = left + right
+        print(f"The answer is {answer}")
+    else:
+        print("Not addition")
     return expr
 
 generate_problem()
