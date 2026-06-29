e5a5b74ecd6a2b9bc31003e42879415acecfa6c0
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-29 15:08:56 +0530

    changed the color scenario

--- WPM-typing/tutorial.py
+++ WPM-typing/tutorial.py
@@ -2,8 +2,10 @@ import curses #helping with styling of the terminal
 from curses import wrapper
 
 def main(stdscr):
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE) # adding styling with foreground green and background white with an id 1
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_WHITE)# Same shi with a second id
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK) # adding styling with foreground green and background white with an id 1
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)# Same shi with a second id
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    
     stdscr.clear() # Clearing the screen
     stdscr.addstr("Hello World!", curses.color_pair(2)) # adding text in to the scrreen
     stdscr.refresh()# Refreshing the screen
