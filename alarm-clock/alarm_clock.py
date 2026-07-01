a65b2b3ceb4ea285797832b8f52b6613e8a48922
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-01 20:05:45 +0530

    added 2 new constant variable to the program so that we can call it once more again

--- alarm-clock/alarm_clock.py
+++ alarm-clock/alarm_clock.py
@@ -1,9 +1,13 @@
 from playsound import playsound
 import time
 
+CLEAR = "\033[2J" # To clear the terminal
+CLEAR_AND_RETURN = "\033[H" #will clear and do the maths in a single line
+
 def alarm(seconds):
     time_elapsed = 0
     
+    print(CLEAR)
     while time_elapsed < seconds:
         time.sleep(1)
         time_elapsed += 1
@@ -13,6 +17,6 @@ def alarm(seconds):
         minutes_left = time_left // 60
         seconds_left = time_left % 60
 
-        print(f"{minutes_left:02d}:{seconds_left:02d}")
+        print(f"{CLEAR_AND_RETURN}{minutes_left:02d}:{seconds_left:02d}")
 
 alarm(10)
