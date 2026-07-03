0d7963fe607757b0eedf8fad2d3a08e845ac725d
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-04 00:01:10 +0530

    feat: add game logic for maze navigation and outcomes

--- js/test.js
+++ js/test.js
@@ -5,11 +5,22 @@ console.log("Hello",name, "Welcome to our game! ")
 const shouldWePlay = prompt('Do you wanna play? ')
 
 if (shouldWePlay.toLowerCase() === "yes") {
-    console.log(`Okay lets play the game ${name}!`);
+    // Game logic
+    const leftOrRight = prompt("You entered a maze, do you want to go left or right? ")
+    if (leftOrRight == "left") {
+        console.log("You go left and see a bridge..,")
+        const cross = prompt("Do you wanna cross the bridge?")
+        if (cross == "yes") {
+            console.log('You crossed, but the bridge was weak and broke and you fell. You lost little breh!')
+        }else{
+            console.log(`Good choice ${name}.. You won the game`)
+        }
+    }else{
+        console.log("You go right and fall of a cliff... Game over buddy!")
+    }
 }else if(shouldWePlay.toLowerCase() == "no") {
     console.log(`Okay ${name} breh! (;`)
 } 
-
 else{
     console.log("Invalid input!..")
 }
