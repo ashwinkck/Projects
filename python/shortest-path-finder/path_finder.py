d45c924ec8c0548b80f6ed06c8149d12423486c2
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-05 00:15:04 +0530

    feat: implement main function to display "Hello World!" in the maze

--- python/shortest-path-finder/path_finder.py
+++ python/shortest-path-finder/path_finder.py
@@ -16,3 +16,14 @@ maze = [
     ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    blue_and_black = curses.color_pair(1)
+    stdscr.clear()
+    stdscr.addstr(5, 5, "Hello World!",blue_and_black)
+    stdscr.refresh()
+    stdscr.getch()
+
+
+wrapper(main)
