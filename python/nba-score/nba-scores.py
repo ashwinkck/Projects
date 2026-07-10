c31ecd5be6b56b9bba164c5ff366c1711f4ddc7b
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-10 22:35:43 +0530

    fix: correct variable usage and improve output formatting in nba-scores.py

--- python/nba-score/nba-scores.py
+++ python/nba-score/nba-scores.py
@@ -4,6 +4,8 @@ from pprint import PrettyPrinter
 BASE_URL = "https://data.nba.net"
 ALL_JSON =  "/prod/v1/today.json"
 
+printer = PrettyPrinter()
+
 # senting a request
-response = get(BASE_URL + ALL_JSON).json()
-print(response)
+data = get(BASE_URL + ALL_JSON).json()
+printer.pprint(data)
