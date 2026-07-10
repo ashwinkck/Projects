7ab97592d159308f5152278a5edcdcedea7b8514
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-10 22:39:44 +0530

    done with the project

--- python/nba-score/nba-scores.py
+++ python/nba-score/nba-scores.py
@@ -1,11 +1,43 @@
 from requests import get
 from pprint import PrettyPrinter
 
-BASE_URL = "https://data.nba.net"
-ALL_JSON =  "/prod/v1/today.json"
+BASE_URL = "https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json"
 
 printer = PrettyPrinter()
 
-# senting a request
-data = get(BASE_URL + ALL_JSON).json()
-printer.pprint(data)
+data = get(BASE_URL).json()
+
+scoreboard = data["scoreboard"]
+
+games = scoreboard["games"]
+
+def getGames():
+    print(scoreboard['gameDate'])
+    print('''-------------------------------------''')
+    for game in games:
+        homeTeam = game['homeTeam']
+        awayTeam = game['awayTeam']
+        homeScore = homeTeam['score']
+        awayScore = awayTeam['score']
+        period = game['period']
+        if period == 0:
+            homeScore = ""
+            awayScore = ""
+        clock = game['gameClock']
+        if clock == "":
+            clock = 0
+        status = game['gameStatusText']
+        leaders = game['gameLeaders']
+        hLeaders = leaders['homeLeaders']
+        aLeaders = leaders['awayLeaders']
+        print(f'''{homeTeam['teamCity']} {homeTeam['teamName']} {homeScore} 
+VS 
+{awayTeam['teamCity']} {awayTeam['teamName']} {awayScore}''')
+        print(f"{status}")
+        if status == "Final":
+            print(f'''Game Leaders: 
+Home: {hLeaders['jerseyNum']} {hLeaders['name']} 
+Away: {aLeaders['jerseyNum']} {aLeaders['name']}''')
+        print('''-------------------------------------''')
+        #break
+getGames()
