16853451ff5b404a0488430e81c751c2d7021d3b
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-11 20:44:11 +0530

    updated with try and except

--- python/automated-file-backup/backup.py
+++ python/automated-file-backup/backup.py
@@ -3,3 +3,17 @@ import shutil
 import datetime
 import schedule
 import time
+
+source_dir = "/home/ash/Pictures"
+destination_dir = "/home/ash/Projects/30days/python/automated-file-backup/backups"
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+    
+
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists inside : {dest}")
