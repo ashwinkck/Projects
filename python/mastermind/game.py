0b9a6bee8530cedf09850472effd946d48c3fe42
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-12 22:47:10 +0530

    still pending

--- python/mastermind/game.py
+++ python/mastermind/game.py
@@ -35,4 +35,14 @@ def check_code(guess, real_code):
     incorrect_pos = 0
 
     for color in real_code:
-        
+        if color not in color_counts:
+            color_counts[color] = 0
+        color_counts[color] += 1
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color == real_color:
+            correct_pos += 1
+            color_counts[guess_color] -= 1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color in color_counts and color_counts[guess_color] > 0:
+            
