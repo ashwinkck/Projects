6aa4a163fb0add909494e90f3863438ac993f5af
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-12 12:02:59 +0530

    feat: update scheduled backup to use direct copy function and add run loop

--- python/automated-file-backup/backup.py
+++ python/automated-file-backup/backup.py
@@ -22,4 +22,8 @@ def l():
     copy_folder_to_directory(source_dir, destination_dir)
 
 #adding features for backup for particular day
-schedule.every().day.at("11:15").do(l)
+schedule.every().day.at("11:15").do(lambda: copy_folder_to_directory(source_dir, destination_dir))
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
