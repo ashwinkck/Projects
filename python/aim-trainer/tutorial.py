b8922e72c62f0565d0ea0b3d76832617e1ac1191
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-14 20:20:05 +0530

    done with draw fn

--- python/aim-trainer/tutorial.py
+++ python/aim-trainer/tutorial.py
@@ -13,6 +13,7 @@ class Target:
     MAX_SIZE = 30
     GROWTH_RATE = 0.2
     COLOR = "red"
+    SECOND_COLOR = "white"
 
     def __init__(self,x,y):
         self.x = x
@@ -30,7 +31,11 @@ class Target:
 
         def draw(self,win):
             pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size) #Drawing a circle
-            
+            pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
+            pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
+            pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
+
+
 
 def main():
     run = True
